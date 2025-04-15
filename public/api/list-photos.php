3e511f7b49b70ac05f16a62e65476e7d077bdd9a
terrasson <monstertaz06@gmail.com>
2025-04-15 23:15:13 +0200

    Correction des erreurs de require dans list-photos.php et direction-photos.php

--- public/api/list-photos.php
+++ public/api/list-photos.php
@@ -1,6 +1,7 @@
 <?php
-require_once 'config.php';
-require_once 'functions.php';
+require_once __DIR__ . '/database.php';
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/functions.php';
 
 header('Content-Type: application/json');
 
